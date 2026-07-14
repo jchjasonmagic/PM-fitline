273fe6e5432918c0e662d2f977e13834262791c5
jichanghong <jichanghong.jason@bytedance.com>
2026-07-14 12:49:40 +0800

    Fix Vite base for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
-    base: process.env.NODE_ENV === 'production' ? '/PM-fitline/' : '/',
+    base: command === 'build' ? '/PM-fitline/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
